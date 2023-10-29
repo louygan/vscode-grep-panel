ec88cdb01c3f1da5542319348c586fa99a327772
Chris <chrisjdavies@users.noreply.github.com>
2023-10-29 16:14:58 +0000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,60 @@
+# Grep Panel for VS Code
+
+![Grep Panel](images/icon.png)
+
+Grep Panel is a Visual Studio Code extension for executing grep searches and
+keeping the results in a panel to browse through and visit.
+
+Three different types of grep are tried in the following order:
+
+1. `rg`: [ripgrep](https://github.com/BurntSushi/ripgrep)
+2. `git grep`: [Git Grep](https://git-scm.com/docs/git-grep)
+3. `grep`: Traditional grep
+
+Whichever succeeds first is used and they're ordered this way from fastest to
+slowest.
+
+## Screenshot
+
+![Screenshot](images/screenshot.png)
+
+## Commands
+
+Grep Panel adds the following commands to the command palette:
+
+- Grep Panel: Grep...
+- Grep Panel: Clear Results
+
+For the _Grep..._ command, the query box is populated with either:
+
+- the selected text, or
+- the word at the cursor if no text is selected.
+
+You will notice that the format of the query includes `-- "..."`.  This is
+because the text is used directly in the command invocation to allow extra flags
+to be passed (e.g. `-i`).  Consequently, if you want to include spaces in your
+pattern, you should surround your query with quotes: e.g. `"hello, world"`.
+
+## Key Bindings
+
+Grep Panel doesn't come with default key bindings, but if you're using
+[VSCodeVim](https://github.com/VSCodeVim/Vim), I personally use:
+
+```json
+{
+    "vim.normalModeKeyBindings": [
+        {
+            "before": ["<C-\>", "r"],
+            "commands": ["grep-panel.grep"]
+        }
+    ]
+}
+```
+
+## Thanks
+
+- Thanks to [ripgrep](https://github.com/BurntSushi/ripgrep) for being an
+  amazingly fast drop-in for `grep`.
+- Thanks to [vim-ripgrep](https://github.com/jremmen/vim-ripgrep) for the
+  inspiration.  I use this all the time in vim and miss it in VS Code.
+- Thanks to DALL-E 3 for generating the icon/logo.
---
+++ src/extension.ts
@@ -0,0 +1,199 @@
+import { execSync } from 'child_process';
+import path = require('path');
+import * as vscode from 'vscode';
+
+// Priority order:
+const GREP_COMMANDS = [
+	"rg -Hn",
+	"git grep -Hn",
+	"grep -Hnr --binary-files=without-match",
+];
+
+const GREP_RESULT_PATTERN = /^([^:]+):(\d+):/;
+
+class GrepResult extends vscode.TreeItem {
+	private path: string;
+	private line: number;
+	private cwd: string;
+
+	constructor(label: string, cwd: string) {
+		super(label, vscode.TreeItemCollapsibleState.None);
+		this.label = label;
+
+		this.cwd = cwd;
+
+		const match = GREP_RESULT_PATTERN.exec(label);
+		this.path = match![1];
+		this.line = parseInt(match![2]);
+
+		this.tooltip = `${this.path}:${this.line}`;
+	}
+
+	public activate(): void {
+		vscode.workspace.openTextDocument(path.join(this.cwd, this.path))
+			.then(document => {
+				vscode.window.showTextDocument(document)
+					.then(editor => {
+						const position = new vscode.Position(this.line - 1, 0);
+						editor.selection = new vscode.Selection(position, position);
+						editor.revealRange(new vscode.Range(position, position));
+					});
+			});
+	}
+}
+
+class GrepResultsTree implements vscode.TreeDataProvider<GrepResult> {
+	private treeView: undefined | vscode.TreeView<GrepResult>;
+	private items: GrepResult[];
+	private _onDidChangeTreeData: vscode.EventEmitter<GrepResult | undefined | null | void> = new vscode.EventEmitter<GrepResult | undefined | null | void>();
+	readonly onDidChangeTreeData: vscode.Event<GrepResult | undefined | null | void> = this._onDidChangeTreeData.event;
+
+	constructor() {
+		this.treeView = vscode.window.createTreeView("grepResults", {"treeDataProvider": this});
+		this.items = [];
+		this.treeView.onDidChangeSelection((event) => {
+			event.selection[0].activate();
+		});
+	}
+
+	getTreeItem(element: GrepResult): vscode.TreeItem | Thenable<vscode.TreeItem> {
+		return element;
+	}
+
+	getChildren(element?: GrepResult | undefined): vscode.ProviderResult<GrepResult[]> {
+		return this.items;
+	}
+
+	setResults(results: GrepResult[]): void {
+		this.items = results;
+		this._onDidChangeTreeData.fire();
+	}
+
+	clearResults(): void {
+		this.items.length = 0;
+		this._onDidChangeTreeData.fire();
+	}
+
+	dispose(): void {
+		this.treeView?.dispose();
+		this.treeView = undefined;
+	}
+}
+
+let grepResults: undefined | GrepResultsTree;
+
+function clearResults(): void {
+	grepResults?.clearResults();
+}
+
+function grepExec(query: string): GrepResult[] {
+	let cwd: string;
+
+	const workspaceFolders = vscode.workspace.workspaceFolders;
+	if (workspaceFolders) {
+		cwd = workspaceFolders[0].uri.fsPath;
+	} else {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return [];
+		}
+
+		cwd = path.dirname(editor.document.uri.fsPath);
+	}
+
+	let cmdIndex = 0;
+	for (let cmdIndex = 0; cmdIndex < GREP_COMMANDS.length; ++cmdIndex) {
+		try {
+			const grepCmd = GREP_COMMANDS[cmdIndex];
+			const grepOutput = execSync(`${grepCmd} ${query} .`, {cwd: cwd});
+			return grepOutput
+				.toString()
+				.split('\n')
+				.filter(v => v.match(GREP_RESULT_PATTERN))
+				.map(v => new GrepResult(v, cwd));
+		}
+		catch (_) {}
+	}
+
+	vscode.window.showErrorMessage("Failed to run any grep command.");
+
+	return [];
+}
+
+function doGrep(query: string): void {
+	grepResults?.setResults(grepExec(query));
+}
+
+function getSelectedText(): string | undefined {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return undefined;
+	}
+
+	const selection = editor.selection;
+	if (selection.isEmpty) {
+		return undefined;
+	}
+
+	return editor.document.getText(selection);
+}
+
+function getWordAtCursor(): string | undefined {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return undefined;
+	}
+
+	const document = editor.document;
+	const selection = editor.selection;
+	const wordRange = document.getWordRangeAtPosition(selection.start);
+
+	if (wordRange) {
+		return editor.document.getText(wordRange);
+	}
+
+	return undefined;
+}
+
+function escapeRegExp(inputString: string): string {
+    return inputString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function doGrepQuery(): void {
+	let val = '';
+
+	const selected = getSelectedText();
+	if (selected) {
+		val = `-- "${escapeRegExp(selected)}"`;
+	} else {
+		const word = getWordAtCursor();
+		if (word) {
+			val = `-- "\\b${escapeRegExp(word)}\\b"`;
+		}
+	}
+
+	vscode.window.showInputBox({
+		placeHolder: 'Grep query',
+		value: val
+	}).then(query => {
+		if (query) {
+			doGrep(query);
+		}
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	grepResults = new GrepResultsTree();
+
+	let commands = [
+		vscode.commands.registerCommand('grep-panel.grep', doGrepQuery),
+		vscode.commands.registerCommand('grep-panel.clear', clearResults)
+	];
+
+	commands.forEach((cmd) => context.subscriptions.push(cmd));
+}
+
+export function deactivate() {
+	grepResults?.dispose();
+	grepResults = undefined;
+}
