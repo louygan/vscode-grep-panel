e63a856d08b6c6fd48a1dbaa234c85143dd9f168
louygan <louygan@users.noreply.github.com>
2024-10-31 16:46:50 +0800

    Create extension.js

---
+++ out/extension.js
@@ -0,0 +1,207 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deactivate = exports.activate = void 0;
+const child_process_1 = require("child_process");
+const path = require("path");
+const vscode = require("vscode");
+const copy = require("copy-paste");
+// Priority order:
+const GREP_COMMANDS = [
+    "rg -Hn",
+    "git grep -Hn",
+    "grep -Hnr --binary-files=without-match",
+];
+const GREP_RESULT_PATTERN = /^([^:]+):(\d+):/;
+class GrepResult extends vscode.TreeItem {
+    constructor(label, cwd) {
+        super(label, vscode.TreeItemCollapsibleState.None);
+        this.label = label;
+        this.cwd = cwd;
+        const match = GREP_RESULT_PATTERN.exec(label);
+        this.path = match[1];
+        this.line = parseInt(match[2]);
+        this.tooltip = `${this.path}:${this.line}`;
+    }
+    activate() {
+        vscode.workspace.openTextDocument(path.join(this.cwd, this.path))
+            .then(document => {
+            vscode.window.showTextDocument(document)
+                .then(editor => {
+                const position = new vscode.Position(this.line - 1, 0);
+                editor.selection = new vscode.Selection(position, position);
+                editor.revealRange(new vscode.Range(position, position));
+            });
+        });
+    }
+    sortGrepResult(a) {
+        return this.path.localeCompare(a.path); // Sort alphabetically by label
+    }
+}
+const sortTreeItems = (a, b) => {
+    return a.sortGrepResult(b);
+};
+class GrepResultsTree {
+    constructor() {
+        this._onDidChangeTreeData = new vscode.EventEmitter();
+        this.onDidChangeTreeData = this._onDidChangeTreeData.event;
+        this.treeView = vscode.window.createTreeView("grepResults", { "treeDataProvider": this });
+        this.items = [];
+        this.treeView.onDidChangeSelection((event) => {
+            event.selection[0].activate();
+        });
+    }
+    getTreeItem(element) {
+        return element;
+    }
+    getChildren(element) {
+        if (!element) {
+            // sort root items
+            return this.items.sort(sortTreeItems);
+        }
+        else {
+            // Sort child items
+            return [];
+        }
+    }
+    getParent(element) {
+        return null;
+    }
+    setResults(results) {
+        this.items = results;
+        this._onDidChangeTreeData.fire();
+    }
+    clearResults() {
+        this.items.length = 0;
+        this._onDidChangeTreeData.fire();
+    }
+    dispose() {
+        this.treeView?.dispose();
+        this.treeView = undefined;
+    }
+    show() {
+        if (this.items.length > 0) {
+            // Set select to false, else we'll jump right to the first result
+            // and that can be quite disorientating.
+            this.treeView?.reveal(this.items[0], { select: false, focus: true });
+        }
+    }
+}
+let grepResults;
+let lastGrepOutput;
+function clearResults() {
+    grepResults?.clearResults();
+    lastGrepOutput = undefined;
+}
+function grepExec(query) {
+    let cwd;
+    const workspaceFolders = vscode.workspace.workspaceFolders;
+    if (workspaceFolders) {
+        cwd = workspaceFolders[0].uri.fsPath;
+    }
+    else {
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return [];
+        }
+        cwd = path.dirname(editor.document.uri.fsPath);
+    }
+    let cmdIndex = 0;
+    let anyRan = false;
+    for (let cmdIndex = 0; cmdIndex < GREP_COMMANDS.length; ++cmdIndex) {
+        try {
+            const grepCmd = GREP_COMMANDS[cmdIndex];
+            const grepOutput = (0, child_process_1.execSync)(`${grepCmd} ${query} *`, { cwd: cwd, maxBuffer: 10 * 1024 * 1024 });
+            lastGrepOutput = grepOutput.toString();
+            return lastGrepOutput
+                .split('\n')
+                .filter(v => v.match(GREP_RESULT_PATTERN))
+                .map(v => new GrepResult(v, cwd));
+        }
+        catch (e) {
+            // An exit status of 1 means the command ran, but produced no
+            // results.  Differentiate between that happening and the case where
+            // no greps were executed successfully.
+            if (e.status === 1) {
+                anyRan = true;
+            }
+        }
+    }
+    if (!anyRan) {
+        vscode.window.showErrorMessage("Failed to run any grep command.");
+    }
+    return [];
+}
+function doGrep(query) {
+    grepResults?.setResults(grepExec(query));
+    grepResults?.show();
+}
+function getSelectedText() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return undefined;
+    }
+    const selection = editor.selection;
+    if (selection.isEmpty) {
+        return undefined;
+    }
+    return editor.document.getText(selection);
+}
+function getWordAtCursor() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return undefined;
+    }
+    const document = editor.document;
+    const selection = editor.selection;
+    const wordRange = document.getWordRangeAtPosition(selection.start);
+    if (wordRange) {
+        return editor.document.getText(wordRange);
+    }
+    return undefined;
+}
+function escapeRegExp(inputString) {
+    return inputString.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+function doGrepQuery() {
+    let val = '';
+    const selected = getSelectedText();
+    if (selected) {
+        val = `-- "${escapeRegExp(selected)}"`;
+    }
+    else {
+        const word = getWordAtCursor();
+        if (word) {
+            val = `-- "\\b${escapeRegExp(word)}\\b"`;
+        }
+    }
+    vscode.window.showInputBox({
+        placeHolder: 'Grep query',
+        value: val
+    }).then(query => {
+        if (query) {
+            doGrep(query);
+        }
+    });
+}
+function copyResults() {
+    if (lastGrepOutput) {
+        copy.copy(lastGrepOutput);
+        vscode.window.showInformationMessage('Results copied to clipboard.');
+    }
+}
+function activate(context) {
+    grepResults = new GrepResultsTree();
+    let commands = [
+        vscode.commands.registerCommand('grep-panel.grep', doGrepQuery),
+        vscode.commands.registerCommand('grep-panel.clear', clearResults),
+        vscode.commands.registerCommand('grep-panel.copy-results', copyResults),
+    ];
+    commands.forEach((cmd) => context.subscriptions.push(cmd));
+}
+exports.activate = activate;
+function deactivate() {
+    grepResults?.dispose();
+    grepResults = undefined;
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
